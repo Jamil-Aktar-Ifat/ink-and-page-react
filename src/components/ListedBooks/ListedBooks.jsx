424763777e8fa0237bbc0d238743fa41c55dd4ed
ifat <i>
2024-08-15 22:39:25 +0100

    listed book section updated

--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -6,12 +6,12 @@ const ListedBooks = () => {
       <h2 className=" text-xl font-bold text-center py-7 rounded-md bg-[#1313130D]">
         Books
       </h2>
-      <div className="text-center my-4 ">
+      <div className="text-center mt-10 mb-24 ">
         <details className="dropdown ">
           <summary className="btn p-4 font-semibold bg-[#23BE0A] text-white">
             Sort By <FaAngleDown></FaAngleDown>
           </summary>
-          <ul className="menu dropdown-content bg-[#1313130D] rounded-box">
+          <ul className="menu dropdown-content bg-[#1313130D] rounded-box w-40 ">
             <li>
               <a>Rating</a>
             </li>
@@ -31,13 +31,13 @@ const ListedBooks = () => {
             name="my_tabs_2"
             role="tab"
             className="tab"
-            aria-label="Tab 1"
+            aria-label="Read Books"
           />
           <div
             role="tabpanel"
             className="tab-content bg-base-100 border-base-300 rounded-box p-6"
           >
-            Tab content 1
+            Read Books
           </div>
 
           <input
@@ -45,14 +45,14 @@ const ListedBooks = () => {
             name="my_tabs_2"
             role="tab"
             className="tab"
-            aria-label="Tab 2"
+            aria-label="Wishlist Books"
             defaultChecked
           />
           <div
             role="tabpanel"
             className="tab-content bg-base-100 border-base-300 rounded-box p-6"
           >
-            Tab content 2
+            Wishlist Books
           </div>
         </div>
       </div>
